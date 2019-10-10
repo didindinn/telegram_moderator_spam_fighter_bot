6f47e46f3509622587e43920a891e70755b8e2d7
Yunzhi Gao <yunzhi@google.com>
2019-10-10 06:28:20 -0700

    mvp

--- moderate.py
+++ moderate.py
@@ -17,6 +17,12 @@ except:
 	ADMINS = {}
 
 try:
+	with open('BOT_OWNER') as f:
+		BOT_OWNER = int(f.read())
+except:
+	BOT_OWNER = 0
+
+try:
 	with open('BLACKLIST') as f:
 		BLACKLIST = f.readlines()
 		BLACKLIST = [x.strip() for x in BLACKLIST]
@@ -80,17 +86,36 @@ def getAuthor(msg):
 		result += ' ' + user.last_name
 	if user.username:
 		result += '(@' + user.username + ')'
-	return result
+	return '[' + result + '](tg://user?id=' + str(user.id) + ')'
+
+def deleteMsg(msg, bot):
+	bot.send_message(chat_id=DEBUG_GROUP, text=getAuthor(msg) + ': ' + (msg.text || ''), parse_mode='Markdown')
+	if msg.photo:
+		# TODO: make this thread safe
+		if msg.photo[0].get_file():
+			msg.photo[0].get_file().download('tmp')
+			bot.send_photo(chat_id=DEBUG_GROUP, photo=open('tmp', 'rb'))
+			os.system('rm tmp')
+	if msg.video:
+		if msg.video.get_file():
+			msg.video.get_file().download('tmp')
+		bot.send_document(chat_id=DEBUG_GROUP, document=open('tmp', 'rb'))
+	bot.delete_message(chat_id=msg.chat_id, message_id=msg.message_id)
+
+def markIfSpam(msg):
+	# Currently only support bot owner
+	if not msg.from_user.id == BOT_OWNER or msg.text != 'spam':
+		return
+	if msg.forward_from:
+		BLACKLIST.add(str(msg.forward_from.id))
+		saveBlacklist()
 
 def handleGroup(update, context):
 	try:
 		msg = update.message
 		if shouldDelete(msg):
-			r = context.bot.send_message(chat_id=DEBUG_GROUP, text=getAuthor(msg) + ': ' + u)
-			context.bot.delete_message(chat_id=msg.chat_id, message_id=msg.message_id)
-
-
-
+			deleteMsg(msg, context.bot)
+		markIfSpam(msg)
 	except Exception as e:
 		print(e)
 		tb.print_exc()
