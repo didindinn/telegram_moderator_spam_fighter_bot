892bd1b1d677084ea37d942943bc967a1b0b3090
Yunzhi Gao <gaoyunzhi@gmail.com>
2019-11-14 18:43:46 -0800

    improve setup

--- setup.py
+++ setup.py
@@ -4,18 +4,25 @@ import sys
 def setup(mode):
 	RUN_COMMAND = 'nohup python3 moderate.py &'
 
-	with open('TOKEN') as f:
-		TOKEN = f.readline().strip()
+	if mode != 'debug':
+		r = os.system('sudo pip3 install -r requirements.txt')
+		if r != 0:
+			os.system('curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py')
+			os.system('sudo python3 get-pip.py')
+			os.system('rm get-pip.py')
+			os.system('sudo pip3 install -r requirements.txt')
 
-	if mode != 'reload':
-		os.system('curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py')
-		os.system('python3 get-pip.py')
-		os.system('rm get-pip.py')
+	try:
+		from telegram.ext import Updater, MessageHandler, Filters
+	except:
+		os.system('sudo pip3 install python-telegram-bot --upgrade') # need to use some experiement feature, e.g. message filtering
 
-		os.system('pip3 install -r requirements.txt')
-		os.system('pip3 install python-telegram-bot --upgrade') # need to use some experiement feature, e.g. message filtering
+	os.system("ps aux | grep ython | grep subscription_v3 | awk '{print $2}' | xargs kill -9")
 
-	return os.system(RUN_COMMAND)
+	if arg.startswith('debug'):
+		os.system(RUN_COMMAND[6:-2])
+	else:
+		os.system(RUN_COMMAND)
 
 
 if __name__ == '__main__':
