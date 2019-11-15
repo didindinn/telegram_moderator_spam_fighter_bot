e1dc90286b990d105c52fef1639af316f02ae68a
Yunzhi Gao <gaoyunzhi@gmail.com>
2019-11-14 20:13:30 -0800

    fix setup

--- setup.py
+++ setup.py
@@ -17,9 +17,9 @@ def setup(mode):
 	except:
 		os.system('sudo pip3 install python-telegram-bot --upgrade') # need to use some experiement feature, e.g. message filtering
 
-	os.system("ps aux | grep ython | grep subscription_v3 | awk '{print $2}' | xargs kill -9")
+	os.system("ps aux | grep ython | grep moderate | awk '{print $2}' | xargs kill -9")
 
-	if arg.startswith('debug'):
+	if mode.startswith('debug'):
 		os.system(RUN_COMMAND[6:-2])
 	else:
 		os.system(RUN_COMMAND)
